d9233ff932a774c6b474156b67e18d9f83de04a3
robertomiranda94 <mirandaroberto1994@gmail.com>
2022-08-18 18:24:15 -0300

    primer commit

---
+++ src/components/Loading.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div className="spinner">
+        <div clasName="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
